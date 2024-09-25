235ce0a83683600e3887e22071e6cd4cc2087c31
saidinesh49 <dineshpalisetty49@gmail.com>
2024-09-25 23:58:05 +0530

    updated middlewares for auth

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/user.model";
+import jwt from 'jsonwebtoken';
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+
+const verifyJwt= asyncHandler( async(req, res, next)=>{
+try {
+        const token= req.cookies?.accessToken || req.header('Authorization')?.replace("Bearer ","");
+        if(!token){
+            throw new ApiError(400, 'UnAuthorized user');
+        }
+    
+        const decodedToken=jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    
+        const user=await User.findById(decodedToken?._id).select("-password -refreshToken");
+    
+        if(!user){
+            throw new ApiError(401, 'User not found');
+        }
+        req.user=user;
+        next();
+} catch (error) {
+    throw new ApiError(error.statusCode || 404, 'Something went wrong during verification');
+  }
+}
+
+);
+
+export { verifyJwt }
