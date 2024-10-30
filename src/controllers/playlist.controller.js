40db48c6b3fcd8952a4ec6defb101c7819d1b076
saidinesh49 <dineshpalisetty49@gmail.com>
2024-10-31 00:35:06 +0530

    update: Playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -12,20 +12,20 @@ const createPlaylist = asyncHandler(async (req, res) => {
         throw new ApiError(400,"Playlist Name and description is required");
     }
 
-    const newPLaylist=await Playlist.create({
+    const newPlaylist=await Playlist.create({
         name: name,
         description: description,
         owner: req.user?._id
     });
 
-    if(!newPLaylist){
+    if(!newPlaylist){
         throw new ApiError(500,"Error while new creating playlist");
     }
 
-    return res.status(200)
+    return res.status(201)
     .json(new ApiResponse(
-        200,
-        newPLaylist,
+        201,
+        newPlaylist,
         "Playlist created successfully"
     ));
 })
@@ -33,8 +33,8 @@ const createPlaylist = asyncHandler(async (req, res) => {
 const getUserPlaylists = asyncHandler(async (req, res) => {
     const {userId} = req.params;
     //TODO: get user playlists
-    if(!userId){
-        throw new ApiError(400,"User ID is required");
+    if(!userId || !isValidObjectId(userId)){
+        throw new ApiError(400,"Invalid User Id");
     }
     const playLists=await Playlist.aggregate([
         {
@@ -80,24 +80,107 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 })
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
+    const {playlistId, videoId} = req.params;
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400, "Invalid playlist id");
+    }
+    const playlist=await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $push:{
+                videos: videoId,
+            }
+        },
+        { new: true }
+    );
+
+    if(!playlist){
+        throw new ApiError(500,"Error while adding video to playlist");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(
+        201,
+        playlist,
+        "Video added to playlist successfully"
+    ));
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
     // TODO: remove video from playlist
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400,"Error while removing video from playlist");
+    }
+
+    const playlist=await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull:{
+                videos: videoId,
+            }
+        },
+        { new: true }
+    );
+
+    if(!playlist){
+        throw new ApiError(500,"Error while removing video from playlist");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(
+        200,
+        playlist,
+        "Video removed from playlist successfully"
+    ));
 
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
-    const {playlistId} = req.params
+    const {playlistId} = req.params;
     // TODO: delete playlist
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400, "Invalid playlist id");
+    }
+
+    await Playlist.deleteOne({ _id: playlistId});
+
+    return res.status(200)
+   .json(new ApiResponse(
+        201,
+        null,
+        "Playlist deleted successfully"
+   ));
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
+    if(!isValidObjectId(playlistId)){
+        throw new ApiError(400, "Invalid playlist id");
+    }
+    const updatedPlaylist=await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set:{
+                name: name,
+                description: description,
+            }
+        },
+        { new: true }
+    );
+
+    if(!updatedPlaylist){
+        throw new ApiError(500, "Error while updating the playlist");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(
+        200,
+        updatedPlaylist,
+        "Playlist updated successfully"
+    ));
 })
 
 export {
