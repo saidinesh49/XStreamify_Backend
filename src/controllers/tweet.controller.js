1d0fcd2e50d8230906ec9e633e5cadaa5ede6da0
saidinesh49 <dineshpalisetty49@gmail.com>
2024-11-05 23:20:41 +0530

    implemented: tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -8,18 +8,88 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body;
+    if(!content){
+        throw new ApiError(400,"Content is required");
+    }
+
+    const newTweet = await Tweet.create({
+        content: content,
+        owner: req.user._id
+    });
+
+    if(!newTweet){
+        throw new ApiError(500,"Error while creating tweet");
+    }
+
+    return res.status(201)
+    .json(new ApiResponse(
+        200,
+        newTweet,
+        "Tweet created successfully"
+    ));
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params;
+    if(!userId){
+        throw new ApiError(400,"Invalid params");
+    }
+
+    const tweets = await Tweet.find({owner: userId});
+
+    return res.status(200)
+    .json(new ApiResponse(
+        200,
+        tweets,
+        "User tweets fetched successfully"
+    ));
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId}=req.params;
+    if(!tweetId){
+        throw new ApiError(400,"Invalid tweet params");
+    }
+
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set:{
+                content: req.body.content,
+            }
+        },
+        { new: true }
+    );
+
+    if(!updateTweet){
+        throw new ApiError(404, "Tweet not found");
+    }
+
+    return res.status(200)
+    .json(new ApiResponse(
+        200,
+        updatedTweet,
+        "Tweet updated successfully"
+    ));
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params;
+    if(!tweetId){
+        throw new ApiError(400, "Invalid tweet id");
+    }
+
+    await Tweet.deleteOne({_id: tweetId});
+    return res.status(200)
+    .json(new ApiResponse(
+        200,
+        null,
+        "Tweet deleted successfully"
+    ));
 })
 
 export {
